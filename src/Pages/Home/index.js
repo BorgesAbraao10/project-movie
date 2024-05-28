7333be9e6b2adb77d2c08527342ff4232c9c2ca5
BorgesAbraao10 <abraao10clear@gmail.com>
2024-05-28 18:44:50 -0300

    Last update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,44 @@
+// useEffect ciclo de vida - useState armazenar em algum estado 
 
-function Home(){
+import { useEffect, useState} from "react";
+import api from '../../services/api';
+
+
+function Home(){    
+    const [filmes, setFilmes] = useState([]);
+
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playng", {
+                params:{
+                    api_key:'1f044149653f684e988c7b4a718ca1f3',
+                    language: 'pt-Br',
+                    page: 1,
+                }
+            })
+
+           // console.log(response.data.results.slice(0,10));
+           setFilmes(response.data.results.slice(0,10))
+
+        }       
+
+        loadFilmes();
+
+    }, [])
+
     return(
-        <div>
-            <h1>Bem vindo a Home</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=> {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import Filme from './Pages/Filme';
 
+import Header from "./components/Header";
+
 function RouteApp(){
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/filme/:id" element={<Filme/>}/>
