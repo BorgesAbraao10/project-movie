8fe7b851a03332a02f4b45eb9aa997801dfef279
BorgesAbraao10 <abraao10clear@gmail.com>
2024-05-30 14:00:58 -0300

    Last update

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import Filme from './Pages/Filme';
 
+import Erro from './Pages/Erro'
+
 import Header from "./components/Header";
 
 function RouteApp(){
@@ -12,6 +14,8 @@ function RouteApp(){
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/filme/:id" element={<Filme/>}/>
+
+                <Route path="*" element={ <Erro/> } />
             </Routes>
         </BrowserRouter>
     )
