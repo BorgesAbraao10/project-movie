cc584338292bb5c12d5c26a077944ab8be22f199
BorgesAbraao10 <abraao10clear@gmail.com>
2024-05-31 11:55:58 -0300

    Last update

--- src/Pages/Filme/index.js
+++ src/Pages/Filme/index.js
@@ -1,9 +1,67 @@
-import './filmes.css';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './filme-info.css';
+
+import api from '../../services/api';
+
+
 
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key:'1f044149653f684e988c7b4a718ca1f3',
+                    language: 'pt-Br',
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("Filme não encontrado")
+            })
+        }
+
+        loadFilme();
+
+        return () =>console.log("Componente foi desmontado")
+
+    }, [])
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                Carregando detalhes...
+            </div>
+        )
+    }
+
+
     return(
-        <div>
-            <h1>Conheça nossos lançamentos!!!!!!!!!!</h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href='#'> 
+                    Trailer
+                    </a>
+                </button>
+            </div>
+
         </div>
     )
 }
